1a372fce33befc97ebfb43e1f8c583b95badc4c5
Omar Faruk <omarfaruk65142@gmail.com>
2023-12-17 14:17:03 +0600

    Complete the header menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
-
+import Header from './components/Header/Header.jsx'
 function App() {
 
 
   return (
-    <>
-      <h2>hello</h2>
-    </>
+    <div>
+      
+      <Header></Header>
+    </div>
   )
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <header className='header-wrapper'>
+                <div className='flexCenter innerWidth paddings header-container'>
+
+                    <img src="/public/logo.png" alt="logo" width={100} />
+                    <div className='header-menu flexCenter'>
+                        <a href="">Residencies</a>
+                        <a href="">Our Value</a>
+                        <a href="">Contact Us</a>
+                        <a href="">Get Started</a>
+                        <button className='button'>
+                            <a href="" style={{color:'white'}}>Contact</a>
+                        </button>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+};
+
+export default Header;
